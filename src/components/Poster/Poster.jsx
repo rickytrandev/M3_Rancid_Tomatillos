e70c8656251e3fde5929aad6d36085edb0b7da37
Ricky Tran <therichardtran@gmail.com>
2024-01-31 21:28:35 -0700

    Create Poster component to render poster images

---
+++ src/components/Poster/Poster.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import './Poster.css'
+
+function Poster({movies}) {
+  const [isMousedOver, setMouseOver] = useState(false)
+
+  function setTrue() {
+    setMouseOver(true)
+  }
+
+  return (
+    <div className="poster-wrapper">
+      {movies.map(movie => (
+        <img onMouseOver={setTrue} className={`poster ${isMousedOver && 'pointer-cursor'}`} src={movie.poster_path} alt={`${movie.title} movie poster`} />
+      ))}
+    </div>
+  )
+}
+
+export default Poster
